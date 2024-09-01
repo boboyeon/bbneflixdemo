3f01cf4635c323193e39c4c176d78c3cc84cc973
boyeon kim <hajoce02@naver.com>
2024-09-02 03:34:38 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import { Route } from 'react-router-dom';
-import './App.css';
-import AppLayout from './layout/AppLayout';
-import Homepage from './pages/Homepage/Homepage';
-import MoviePage from './pages/movies/MoviePage';
-import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
-
+import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 // 홈페이지 = /
 // 영화 전체를 보여주는 페이지 (서치) = /movies
 // 영화 디테일 페이지 = /movies/:id
 
-
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<AppLayout/>}>
-      <Route index element={<Homepage/>}></Route>
-      <Route path='/movies' element={<MoviePage/>}></Route>
-      <Route path='/movies/:id' element={<MovieDetailPage/>}></Route>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />}></Route>
+        {/* 연관된 주소를 묶어서 구성한 라우트 (movies) */}
+        <Route path="movies">
+          <Route index element={<MoviePage />}></Route>
+          <Route path=":id" element={<MovieDetailPage/>}></Route>
+        </Route>
+        {/* <Route path='/movies' element={<MoviePage/>}></Route>
+      <Route path='/movies/:id' element={<MovieDetailPage/>}></Route> 
+      => 라우트를 구성하는 기본 */}
+
+
+        <Route path="*" element={<NotFoundPage/>}></Route>
+
       </Route>
     </Routes>
   );
