6bdd0ddcc1a983a9659a30c4a0830a3a29ce3452
boyeon kim <hajoce02@naver.com>
2024-09-03 02:47:07 +0900

    5th commit

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+import "./Banner.style.css";
 
 const Banner = () => {
-  return (
-    <div>
-      banner
-    </div>
-  )
-}
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+  console.log("ddd", data);
+  if (isLoading) {
+    return (
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
 
-export default Banner
+  if (isError) {
+    return (
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <Alert variant="danger">
+          <p>{error.message}</p>
+        </Alert>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w533_and_h300_bestv2${data?.results[0].poster_path}` +
+          ")",
+      }}
+      className="banner"
+    >
+      <div className="text-white banner-text-area">
+        <h1>{data?.results[0].title}</h1>
+        <p>{data?.results[0].overview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
